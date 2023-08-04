f95903441af6cd55f371a25d55218697a7a160e3
Max Milton <max@happyhackers.xyz>
2023-08-05 00:58:18 +1000

    chore: Rework lint config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -13,32 +13,41 @@ module.exports = {
   },
   extends: [
     'eslint:recommended',
-    'plugin:@typescript-eslint/eslint-recommended',
     'airbnb-base',
     'airbnb-typescript/base',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+    'plugin:@typescript-eslint/strict-type-checked',
+    'plugin:@typescript-eslint/stylistic-type-checked',
     'plugin:unicorn/recommended',
     'prettier',
   ],
   plugins: ['prettier'],
   rules: {
     '@typescript-eslint/explicit-module-boundary-types': ERROR,
+    '@typescript-eslint/no-confusing-void-expression': WARN,
+    '@typescript-eslint/no-non-null-assertion': WARN,
+    'import/order': OFF, // broken with prettier
     'import/prefer-default-export': OFF,
-    'no-plusplus': OFF,
     'no-restricted-syntax': OFF,
     'no-void': OFF,
     'prettier/prettier': WARN,
     'unicorn/filename-case': OFF,
     'unicorn/no-abusive-eslint-disable': WARN,
-    'unicorn/no-for-loop': OFF, // used in performance critical loops
     'unicorn/no-null': OFF,
     'unicorn/prefer-at': OFF, // browser compatibility issues
-    'unicorn/prefer-code-point': OFF, // worse performance
     'unicorn/prefer-module': WARN,
-    'unicorn/prefer-spread': OFF, // string spread is way slower than split('')
     'unicorn/prefer-top-level-await': WARN,
     'unicorn/prevent-abbreviations': OFF,
+
+    /* Performance and byte savings */
+    'no-plusplus': OFF,
+    // not always faster
+    'unicorn/prefer-set-has': WARN,
+    // used in performance critical loops
+    'unicorn/no-for-loop': OFF,
+    // worse performance
+    'unicorn/prefer-code-point': OFF,
+    // string spread is way slower than split('')
+    'unicorn/prefer-spread': OFF,
     'unicorn/switch-case-braces': [ERROR, 'avoid'],
   },
 };
