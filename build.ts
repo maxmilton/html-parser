2036291e51c7d80855473a12d5dd931206296035
Max Milton <max@happyhackers.xyz>
2025-05-28 21:18:58 +0900

    chore: Simplify build process

---
+++ build.ts
@@ -0,0 +1,28 @@
+/* eslint-disable no-console */
+
+import { createBundle } from 'dts-buddy';
+
+console.time('prebuild');
+await Bun.$`rm -rf dist`;
+console.timeEnd('prebuild');
+
+console.time('build');
+const out = await Bun.build({
+  entrypoints: ['src/index.ts'],
+  outdir: 'dist',
+  minify: true,
+  sourcemap: 'linked',
+});
+console.timeEnd('build');
+console.log(out.outputs);
+
+console.time('dts');
+await createBundle({
+  project: 'tsconfig.json',
+  output: 'dist/index.d.ts',
+  modules: {
+    '@maxmilton/html-parser': 'src/index.js',
+  },
+  include: ['src'],
+});
+console.timeEnd('dts');
