0e0dcf2d2029ba8376fb87bec901b779831cabfb
Max Milton <max@happyhackers.xyz>
2025-05-28 21:16:38 +0900

    chore: Rename and tweak ESLint config

--- eslint.config.js
+++ eslint.config.js
@@ -11,7 +11,7 @@ export default ts.config(
   eslint.configs.recommended,
   ...ts.configs.strictTypeChecked,
   ...ts.configs.stylisticTypeChecked,
-  unicorn.configs['flat/recommended'],
+  unicorn.configs.recommended,
   mm.configs.recommended,
   {
     linterOptions: {
@@ -19,10 +19,7 @@ export default ts.config(
     },
     languageOptions: {
       parserOptions: {
-        project: ['tsconfig.json', 'tsconfig.node.json'],
-        projectService: {
-          allowDefaultProject: ['*.mjs'],
-        },
+        projectService: true,
         tsconfigRootDir: import.meta.dirname,
       },
     },
